f17133b565a10445f07dff5d01b80f954459452c
Masaki KOMAGATA <komagata@gmail.com>
2010-05-02 20:34:54 +0900

    fixed

--- public/menu.php
+++ public/menu.php
@@ -1,6 +1,6 @@
   <div id="menu">
     <ul>
-      <li><a href="about.php">イベントの詳細</a></li>
+      <li><a href="program.php">プログラム</a></li>
       <li><a href="subscription.php">お申し込み</a></li>
       <li><a href="inquiry.php">お問い合わせ</a></li>
     </ul>
